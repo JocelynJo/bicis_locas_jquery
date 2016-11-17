dd01bc2a16e13d2df7c8d0c3b11910f2f420965c
JocelynJoe <jocelyn.pena.contreras@gmail.com>
2016-11-17 09:52:05 -0800

    correccion lista

--- js/main.js
+++ js/main.js
@@ -7,13 +7,15 @@ function validateForm(){
 		$('.name-container').append(spanNuevo);
 		return false; 
 	}else if(!validarLetras(name)){
+		$('.nom').fadeOut();
 		var spanNuevo1=$('<span class="nom">Desde ingresar solo letras</span>');
 		$('.name-container').append(spanNuevo1);
-		return false; 
+		return false;
 	}else if(!validarMayus(name)){
+		$('.nom').fadeOut();
 		var spanNuevo2=$('<span class="nom">Debe comenzar con mayuscula</span>');
 		$('.name-container').append(spanNuevo2);
-		return false; 
+		return false;	
 	}else{
 		$('.nom').fadeOut();
 	}
@@ -21,14 +23,17 @@ function validateForm(){
 
 	var lastname= $('#lastname').val();
 	if(lastname.length==0|| lastname==null){
+		$('.ape').fadeOut();
 		var spanLastname=$('<span class="ape">Este campo es obligatorio</span>');
 		$('.lastname-container').append(spanLastname);
 		return false; 
 	}else if(!validarLetras(lastname)){
+		$('.ape').fadeOut();
 		var spanLastname1=$('<span class="ape">Desde ingresar solo letras</span>');
 		$('.lastname-container').append(spanLastname1);
 		return false; 
 	}else if(!validarMayus(lastname)){
+		$('.ape').fadeOut();
 		var spanLastname2=$('<span class="ape">Debe comenzar con mayuscula</span>');
 		$('.lastname-container').append(spanLastname2);
 		return false; 
@@ -64,7 +69,7 @@ function validateForm(){
 			$('.em').fadeOut(); 
 			return true;
 		}else{
-			$('span').fadeOut(); 
+			$('.em').fadeOut(); 
 			var spanPass2=$('<span class="em">Su contraseña debe tener mas de 6 caracteres</span>')
 			$('#input-password').parent().append(spanPass2);
 		}
@@ -76,7 +81,7 @@ function validateForm(){
 	//validar lista!!!!
 	var selecBici = $('select').val();
 		if(selecBici == 0){
-			var biciContiene=$('form-group:eq(1)');//seleccona el ellemento que esta en el indice 2 desde el form-group
+			var biciContiene=$('.form-group:eq(1)');//seleccona el ellemento que esta en el indice 2 desde el form-group
 			biciContiene.append('<span class="err">Seleciona una bici!</span>'); 
 			return false;
 		}else{
@@ -95,10 +100,7 @@ function validateForm(){
 }
 
 
-<<<<<<< HEAD
-=======
-//funciones paea validar nombre y apellido
->>>>>>> master
+
 function validarLetras(nombre){
 	var filter6=/^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
 	if (filter6.test(nombre)){
@@ -107,6 +109,7 @@ function validarLetras(nombre){
 	   return false;
    }
 }
+
 function validarMayus(nombre){
 	if(nombre.substring(0,1)==nombre.substring(0,1).toUpperCase()){
 		return true;
